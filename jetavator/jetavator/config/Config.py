affb084e7dc18e51f75e861b90064072ec0fe639
Joe Taylor <joetaylorconsulting@gmail.com>
2020-06-12 23:11:23 +0200

    Upgrade to wysdom 0.1.4

--- jetavator/jetavator/config/Config.py
+++ jetavator/jetavator/config/Config.py
@@ -37,8 +37,6 @@ class DBServiceConfig(ServiceConfig):
             return wysdom.document(self).schema
 
     type: str = ConfigProperty(str)
-    # TODO: Update jso behaviour so default and default_function
-    #       don't set a value persistently
     schema: str = ConfigProperty(str, default_function=_get_default_schema)
 
 
@@ -63,17 +61,8 @@ class SessionConfig(wysdom.UserObject):
 
 class Config(wysdom.UserObject, wysdom.ReadsJSON, wysdom.ReadsYAML):
 
-    # TODO: Move this to testing framework - this isn't a feature
-    #       required outside of self-testing
-    def _generate_uid(self):
-        return self.prefix + "_" + "_".join([
-            "%04x" % random.randrange(16**4)
-            for i in range(0, 3)
-        ])
-
     model_path: str = ConfigProperty(str, default_function=lambda self: os.getcwd())
-    schema: str = ConfigProperty(str, default_function=_generate_uid)
-    prefix: str = ConfigProperty(str, default="jetavator")
+    schema: str = ConfigProperty(str)
     drop_schema_if_exists: bool = ConfigProperty(bool, default=False)
     skip_deploy: bool = ConfigProperty(bool, default=False)
     environment_type: str = ConfigProperty(str, default="local_spark")
