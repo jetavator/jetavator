c6776db783b91918df1417267371641af085a478
Joe Taylor <joetaylorconsulting@gmail.com>
2020-12-24 13:49:12 +0100

    Use new SQL DML MERGE feature in Delta Lake 0.7

--- jetavator/jetavator/runners/spark_runner/jobs/StarMerge.py
+++ jetavator/jetavator/runners/spark_runner/jobs/StarMerge.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import List, Tuple, Iterator
 
-from jetavator.schema_registry import SatelliteOwner
+from jetavator.schema_registry import SatelliteOwner, Satellite
 
-from .. import SparkJob, SparkRunner
+from .. import SparkJob, SparkSQLJob, SparkRunner
 
 
-class StarMerge(SparkJob, register_as='star_merge'):
+class StarMerge(SparkSQLJob, register_as='star_merge'):
     """
     Merges the created, updated or deleted rows into the the star schema
     Delta Lake table for this particular `Hub` or `Link`.
@@ -14,6 +14,27 @@ class StarMerge(SparkJob, register_as='star_merge'):
     :param satellite_owner: The `Hub` or `Link` object that is being used to create
                             a Dimension or Fact table, respectively.
     """
+
+    sql_template = '''
+        MERGE 
+         INTO {{ job.satellite_owner.star_table_name }} AS target
+        USING {{ job.star_data_job.name }} AS source
+           ON target.{{ job.satellite_owner.key_column_name }}
+            = source.{{ job.satellite_owner.key_column_name }}
+         WHEN MATCHED AND source.deleted_ind = 1 THEN DELETE
+         {% for column, satellite in job.star_column_references %}
+         {{ "WHEN MATCHED THEN UPDATE SET" if loop.first }}
+             {{ column }} = CASE WHEN array_contains(
+                 source.key_source,
+                 '{{ satellite.full_name }}'
+             )
+             THEN source.{{ column }}
+             ELSE target.{{ column }}
+             END
+           {{ "," if not loop.last }}
+         {% endfor %}
+         WHEN NOT MATCHED THEN INSERT *
+        '''
 
     def __init__(
             self,
@@ -27,59 +48,22 @@ class StarMerge(SparkJob, register_as='star_merge'):
     def name(self) -> str:
         return f'merge_{self.satellite_owner.star_table_name}'
 
-    def execute(self):
-        path = [
-            row
-            for row in self.spark.sql(
-                'DESCRIBE FORMATTED ' +
-                self.satellite_owner.star_table_name
-            ).collect()
-            if row['col_name'] == 'Location'
-        ][0]['data_type']
-
-        source = self.spark.table(self.star_data_job.name)
-
-        # Import has to happen inline because delta library is installed
-        # at runtime by PySpark. Not ideal as not PEP8 compliant!
-        # Create a setuptools-compatible mirror repo instead?
-
-        # noinspection PyUnresolvedReferences
-        from delta.tables import DeltaTable
-
-        (
-            DeltaTable
-            .forPath(self.spark, path)
-            .alias('target').merge(
-                source.alias('source'),
-                (
-                    f'target.{self.satellite_owner.key_column_name}'
-                    f' = source.{self.satellite_owner.key_column_name}'
-                )
-            )
-            .whenMatchedDelete(condition='source.deleted_ind = 1')
-            .whenMatchedUpdate(set={
-                column: f"""
-                    CASE WHEN array_contains(
-                            source.key_source,
-                            '{satellite.full_name}'
-                         )
-                         THEN source.{column}
-                         ELSE target.{column}
-                         END
-                    """
-                for satellite in self.satellite_owner.star_satellites.values()
-                for column in satellite.columns.keys()
-            })
-            .whenNotMatchedInsertAll()
-            .execute()
-        )
-
     @property
     def star_data_job(self) -> SparkJob:
         """
         :return: The `StarData` job that contains the updated keys and data.
         """
         return self.runner.get_job('star_data', self.satellite_owner)
+
+    @property
+    def star_column_references(self) -> Iterator[Tuple[str, Satellite]]:
+        """
+        :return: An iterator of tuples containing column names in the star schema
+                 and their owning satellites.
+        """
+        for satellite in self.satellite_owner.star_satellites.values():
+            for column in satellite.columns.keys():
+                yield column, satellite
 
     @property
     def dependencies(self) -> List[SparkJob]:
